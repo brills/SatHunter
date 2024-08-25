5820a3de58c2460bbb233b079d636ea1b071d293
Zhuo Peng <1835738+brills@users.noreply.github.com>
2024-08-24 20:37:34 -0700

    Set up Github action to run UITests (#4)
    * Added a UITest
    
    * Added a github action to run UI tests
    
    * typo
    
    * install protobuf
    
    * typo
    
    * use checkout@v4

---
+++ SatHunterUITests/SatHunterUITests.swift
@@ -0,0 +1,27 @@
+//
+//  SatHunterUITests.swift
+//  SatHunterUITests
+//
+//  Created by Zhuo Peng on 8/24/24.
+//
+
+import XCTest
+
+final class SatHunterUITests: XCTestCase {
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
